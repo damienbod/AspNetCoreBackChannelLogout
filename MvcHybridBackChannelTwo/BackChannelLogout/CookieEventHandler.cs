82b44278170f6283c77ec583219c2447fb6d79f0
damienbod <damien_bod@hotmail.com>
2021-02-03 13:50:26 +0100

    updating logging

--- MvcHybridBackChannelTwo/BackChannelLogout/CookieEventHandler.cs
+++ MvcHybridBackChannelTwo/BackChannelLogout/CookieEventHandler.cs
@@ -1,22 +1,26 @@
 ﻿using Microsoft.AspNetCore.Authentication.Cookies;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
 
 namespace MvcHybridBackChannelTwo.BackChannelLogout
 {
     public class CookieEventHandler : CookieAuthenticationEvents
     {
         private readonly LogoutSessionManager _logoutSessionManager;
+        private readonly ILogger<CookieEventHandler> _logger;
 
-        public CookieEventHandler(LogoutSessionManager logoutSessions)
+        public CookieEventHandler(LogoutSessionManager logoutSessions, ILoggerFactory loggerFactory)
         {
             _logoutSessionManager = logoutSessions;
+            _logger = loggerFactory.CreateLogger<CookieEventHandler>();
         }
 
         public override async Task ValidatePrincipal(CookieValidatePrincipalContext context)
         {
             if (context.Principal.Identity.IsAuthenticated)
             {
+                _logger.LogDebug($"ValidatePrincipal: {context.Principal.Identity.IsAuthenticated}");
                 var sub = context.Principal.FindFirst("sub")?.Value;
                 var sid = context.Principal.FindFirst("sid")?.Value;
 
